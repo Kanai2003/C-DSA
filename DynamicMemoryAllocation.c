1823e129020484c70a2d6b8b6e80c7c85815ceeb
kanailalmanna <kanailalmanna2003@gmail.com>
2022-03-15 22:31:34 +0530

    basic syntex

---
+++ DynamicMemoryAllocation.c
@@ -0,0 +1,22 @@
+/*  
+"malloc()"->memory allocation 
+"calloc()"->continuous allocation 
+"free()"-> it is use to totally free memory 
+"realloc()"->re-allocation memory
+*/
+
+# include <stdio.h>
+# include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int *ptr;
+    ptr = (int *) malloc(5 * sizeof(int));  // (5 * sizeof(int)) is number or size in bytes
+
+    ptr = (int *) calloc(5,sizeof(int));  
+
+    ptr = realloc(ptr, 5);
+
+
+    return 0;
+}
