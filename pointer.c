94c05f8149ebb2ef97057b6478a23d0c4c796929
kanailal <kanailalmanna2003@gmail.com>
2022-04-23 11:01:53 +0530

    added to master

--- pointer.c
+++ pointer.c
@@ -1,10 +1,11 @@
-# include<stdio.h>;
+# include<stdio.h>
 int main()
 {
     int age = 18;
     int *ptr = &age;               //"*" is dinoting value of "ptr" and '&' is denoting address of "age"  
     printf("Address of age is : %u \n",ptr);
     printf("Value of age is : %d\n", age);
+    printf("Value of age is : %d\n", *ptr);
     printf("%u",*(&ptr));
     
 
