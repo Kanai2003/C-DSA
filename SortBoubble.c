cc7015cc88c0b012c4366674b344dbf4ef3aa642
kanailalmanna <kanailalmanna2003@gmail.com>
2022-05-17 01:44:28 +0530

    add to madter

--- SortBoubble.c
+++ SortBoubble.c
@@ -12,20 +12,22 @@ int main (){
     }
 
     //boubble sorting
-    // int flag;
+    int flag;
     for (int i = 0; i<size-1 ; i++){
         for(int j =0; j<size-1-i ; j++){
-            // flag = 0;                            
+            flag = 0;                            
             if(arr[j]>arr[j+1]){
                 int temp = arr[j];
                 arr[j]=arr[j+1];
                 arr[j+1]=temp;
-                // flag = 1;
+                flag = 1;
             }
+            
         }
-        // if(flag == 0){
-        //     break;
-        // }
+        if(flag == 0){
+                break;
+            }
+        
     }
 
     //printing array
