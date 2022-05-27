512f560da742e978b41d7d541f6c2aa25db7ebf4
kanailalmanna <kanailalmanna2003@gmail.com>
2022-05-27 07:58:09 +0530

    add to master

--- array.c
+++ array.c
@@ -1,5 +1,44 @@
 #include<stdio.h>
 int main(){
-    
+    int size;
+    printf("Enter size of array : ");
+    scanf("%d",&size);
+    int arr[size] , rem , num ;
+    printf("Enter number (<%d) of vallue to give as input : ",size);
+    scanf("%d",&num);
+    for(int i=0; i<num ; i++){
+        printf("Enter Array[%d] : ",i);
+        scanf("%d",&arr[i]);
+    }
+    rem = size - num;
+    printf("No. of element present = %d \nRemaining %d\nDo you want to take input ? (y/n) : ",num,rem);
+    char per;
+    scanf("%c",&per);
+    scanf("%c",&per);
+    switch(per){
+        case 'n':
+        case 'N':
+            for(int i =0; i<num; i++){
+                printf("%d ",arr[i]);
+            }
+            break;
+        
+        case 'Y':
+        case 'y':
+            printf("Remaining only %d\nHow much ? :",rem);
+            scanf("%d",&num);
+            if(num<=rem){
+                for(int i = size-rem;i<size;i++){
+                    printf("Array[%d] : ",i);
+                    scanf("%d",&arr[i]);
+                }
+                rem -= num ;
+            }else printf("Invalid input \n");
+            for(int i =0; i<size-rem;i++){
+                printf("%d ",arr[i]);
+            }
+            break;
+    }
+
     return 0;
 }
