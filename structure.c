feeb8d8f0cccf8fd96dd183daa529c91c34e6913
kanailalmanna <kanailalmanna2003@gmail.com>
2022-06-09 09:42:38 +0530

    add to master

--- structure.c
+++ structure.c
@@ -1,24 +1,63 @@
-#include <stdio.h>
+// #include <stdio.h>
 
+// struct student{
+//     char name[20];
+//     int uroll;
+//     float cgpa;
+// };
+
+
+
+// int main(){
+
+
+//     struct student kanai;
+//     printf("Enter name \n");
+//     scanf("%[^\n]s %d %f",kanai.name);
+//     printf("Enter uroll \n");
+//     scanf("%d",&kanai.uroll);
+//     printf("Enter cgpa \n");
+//     scanf("%f",&kanai.cgpa);
+    
+//     printf("%s  %d  %.2f\n",kanai.name,kanai.uroll,kanai.cgpa);
+//     return 0;
+// }
+
+
+
+
+#include<stdio.h>
 struct student{
-    char name[20];
-    int uroll;
+    int roll;
     float cgpa;
 };
 
-
-
-int main(){
-
-
-    struct student kanai;
-    printf("Enter name \n");
-    scanf("%[^\n]s %d %f",kanai.name);
-    printf("Enter uroll \n");
-    scanf("%d",&kanai.uroll);
-    printf("Enter cgpa \n");
-    scanf("%f",&kanai.cgpa);
+void main(){
+    typedef struct student tint;
+    tint x[3];
+    for (int i = 0; i < 3; i++){
+        printf("\nEnter roll : ");
+        scanf("%d",&x[i].roll);
+        printf("\nEnter cgpa : ");
+        scanf("%f",&x[i].cgpa);
+    }
     
-    printf("%s  %d  %.2f\n",kanai.name,kanai.uroll,kanai.cgpa);
-    return 0;
+    for (int  i = 0; i < 3; i++){
+        for (int  j = 0; i < 3-i-1; j++){
+            if(x[j].cgpa <x[j+1].cgpa){
+                int temp = x[j].cgpa;
+                x[j].cgpa = x[j+1].cgpa;
+                x[j+1].cgpa = temp;
+
+                temp = x[j].roll;
+                x[j].roll = x[j+1].roll;
+                x[j+1].roll = temp;
+            }
+        } 
+    }
+    for (int  i = 0; i < 3; i++){
+        printf("\n %d  %.2f",x[i].roll,x[i].cgpa);
+    }
+    
+    
 }
