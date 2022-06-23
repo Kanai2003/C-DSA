8adb0e0c50f829377b76c3673172258ca69f6c08
kanailalmanna <kanailalmanna2003@gmail.com>
2022-06-23 23:22:51 +0530

    add to master

--- structure.c
+++ structure.c
@@ -1,30 +1,22 @@
 // #include <stdio.h>
-
 // struct student{
 //     char name[20];
 //     int uroll;
 //     float cgpa;
 // };
 
-
-
 // int main(){
-
-
 //     struct student kanai;
+//     struct student classX[10];       //we can make this type of array
 //     printf("Enter name \n");
-//     scanf("%[^\n]s %d %f",kanai.name);
+//     scanf("%[^\n]s",kanai.name);
 //     printf("Enter uroll \n");
 //     scanf("%d",&kanai.uroll);
 //     printf("Enter cgpa \n");
 //     scanf("%f",&kanai.cgpa);
-    
 //     printf("%s  %d  %.2f\n",kanai.name,kanai.uroll,kanai.cgpa);
 //     return 0;
 // }
-
-
-
 
 #include<stdio.h>
 struct student{
