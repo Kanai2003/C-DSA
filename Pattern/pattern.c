54b8ed84a8a7946fd83561474706d883b5952b3a
Kanailal Manna <91842029+KanailalManna@users.noreply.github.com>
2022-08-31 00:43:31 +0530

    add to master

--- Pattern/pattern.c
+++ Pattern/pattern.c
@@ -1,12 +1,22 @@
 #include<stdio.h>
 int main(){
-    // int a = 5,i,j;
-    // for( i = 1; i <= a; i++){
-    //    for(j = 1 ; j <= i; j++){
-    //        printf("%d",j);
-    //    }
-    //    printf("\n");
-    // }
+    int n;
+    printf("Enter number of lines : ");
+    scanf("%d",&n);
+    for(int i =1; i<=n; i++){
+        if(i == 1 || i== n){
+            for(int j=1;j<=n;j++){
+                printf("*");
+            }
+        }else{
+            printf("*");
+            for(int j=1;j<=n-2;j++){
+                printf(" ");
+            }
+            printf("*");
+        }
+        printf("\n");
+    }
 
     return 0;
 }       
