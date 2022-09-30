8063218a697c18afa5f53321d80edf59322abcfc
Kanailal Manna <91842029+KanailalManna@users.noreply.github.com>
2022-09-30 19:56:13 +0530

    add to master

--- eligibleForVote.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-    printf("Enter your age to check your voting eligibility : ");
-    int a;
-    scanf("%d",&a);
-    if(a>=18){
-        printf("You are eligible for voting :) ");
-    }else printf("You are not eligible for voting :( ");
-}
