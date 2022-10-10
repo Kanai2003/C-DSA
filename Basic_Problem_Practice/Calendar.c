f0402a577fc85938d0786107afe38b65126045c9
Subhojit Ghosh <96782328+Subhojit666@users.noreply.github.com>
2022-10-10 19:56:29 +0530

    New calendar

--- Basic_Problem_Practice/Calendar.c
+++ Basic_Problem_Practice/Calendar.c
@@ -1,85 +1,125 @@
-#include<stdio.h>
-#include<conio.h>
-#define TRUE 1
-#define FALSE 0
-int days_in_month[]={0,31,28,31,30,31,30,31,31,30,31,30,31};
-char *months[]=
+#include <stdio.h>
+int dayNumber(int day, int month, int year)
 {
-    " ",
-    "\n\n\nJanuary",
-    "\n\n\nFebruary",
-    "\n\n\nMarch",
-    "\n\n\nApril",
-    "\n\n\nMay",
-    "\n\n\nJune",
-    "\n\n\nJuly",
-    "\n\n\nAugust",
-    "\n\n\nSeptember",
-    "\n\n\nOctober",
-    "\n\n\nNovember",
-    "\n\n\nDecember"
-};
-int inputyear(void)
-{
-    int year;
-    printf("Enter a year: ");
-    scanf("%d",&year);
-    return year;
+  static int t[] = { 0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4 };
+	year -= month < 3;
+	return (year + year / 4
+			- year / 100
+			+ year / 400
+			+ t[month - 1] + day)
+		% 7;
 }
-int determineleapyear(int year)
+char* getMonthName(int monthNumber)
 {
-    if(year%4==FALSE && year %100!=FALSE || year%400==FALSE)
-    {
-        days_in_month[2]=29;
-        return TRUE;
-    }
-    else
-    {
-        days_in_month[2]=28;
-        return FALSE;
-    }
-}
-int determinedaycode(int year)
-{
-    int daycode;
-    int d1,d2,d3;
+	char* month;
 
-    d1=(year-1.)/4.0;
-    d2=(year-1.)/100.;
-    d3=(year-1.)/400.;
-    daycode=(year+d1-d2+d3)%7;
-    return daycode;
+	switch (monthNumber) {
+	case 0:
+		month = "January";
+		break;
+	case 1:
+		month = "February";
+		break;
+	case 2:
+		month = "March";
+		break;
+	case 3:
+		month = "April";
+		break;
+	case 4:
+		month = "May";
+		break;
+	case 5:
+		month = "June";
+		break;
+	case 6:
+		month = "July";
+		break;
+	case 7:
+		month = "August";
+		break;
+	case 8:
+		month = "September";
+		break;
+	case 9:
+		month = "October";
+		break;
+	case 10:
+		month = "November";
+		break;
+	case 11:
+		month = "December";
+		break;
+	}
+	return month;
 }
-void calender(int year,int daycode)
+int numberOfDays(int monthNumber, int year)
 {
-    int month,day;
-    for(month=1;month <=12;month++)
-    {
-        printf("%s",months[month]);
-        printf("\n\nSun Mon Tue Wed Thu Fri Sat\n");
-        for(day=1;day<=daycode*4;day++)
-        {
-            printf(" ");
-        }
-        for(day=1;day<=days_in_month[month];day++)
-        {
-            printf("%2d",day);
-            if((day+daycode)%7>0)
-                printf("  ");
-            else
-                printf("\n");
-        }
-            
-        daycode=(daycode+days_in_month[month])%7;
-    }
+	if (monthNumber == 0)
+		return (31);
+	if (monthNumber == 1) {
+		if (year % 400 == 0
+			|| (year % 4 == 0
+				&& year % 100 != 0))
+			return (29);
+		else
+			return (28);
+	}
+	if (monthNumber == 2)
+		return (31);
+	if (monthNumber == 3)
+		return (30);
+	if (monthNumber == 4)
+		return (31);
+	if (monthNumber == 5)
+		return (30);
+	if (monthNumber == 6)
+		return (31);
+	if (monthNumber == 7)
+		return (31);
+	if (monthNumber == 8)
+		return (30);
+	if (monthNumber == 9)
+		return (31);
+	if (monthNumber == 10)
+		return (30);
+	if (monthNumber == 11)
+		return (31);
 }
-int main(void)
+void printCalendar(int year)
 {
-    int year,daycode,leapyear;
-    year=inputyear();
-    daycode=determinedaycode(year);
-    determineleapyear(year);
-    calender(year,daycode);
-    printf("\n");
-    return 0;
+	printf("	 Calendar - %d\n\n", year);
+	int days;
+	int current = dayNumber(1, 1, year);
+	for (int i = 0; i < 12; i++) {
+		days = numberOfDays(i, year);
+		printf("\n ------------%s-------------\n",
+			getMonthName(i));
+		printf(" Sun Mon Tue Wed Thu Fri Sat\n");
+		int k;
+		for (k = 0; k < current; k++)
+			printf("	 ");
+
+		for (int j = 1; j <= days; j++) {
+			printf("%5d", j);
+
+			if (++k > 6) {
+				k = 0;
+				printf("\n");
+			}
+		}
+
+		if (k)
+			printf("\n");
+
+		current = k;
+	}
+
+	return;
+}
+int main()
+{
+	int year = 2016;
+	printCalendar(year);
+	return 0;
 }
